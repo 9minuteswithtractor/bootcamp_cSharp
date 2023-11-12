78a25ac471c5a87161eceb038dc38184139eb103
Peter <>
2023-11-12 23:38:46 +0200

    newFeat : Parse data

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -23,16 +23,45 @@ namespace FirstApp
             Console.ForegroundColor = ConsoleColor.Black;
             Console.BackgroundColor = ConsoleColor.White;
             // TODO: Prompt user for its name and save it to string variable 
-            Console.Write("\n\tEnter Your name: ");
-            string userName = Console.ReadLine();
+            //Console.Write("\n\tEnter Your name: ");
+            //string userName = Console.ReadLine();
             ////////////////////////////////////////////////// 
 
 
             Console.ForegroundColor = ConsoleColor.Black;
             Console.BackgroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine($"\tIt's great to meet You {userName}!");
+            //Console.WriteLine($"\tIt's great to meet You {userName}!");
+
+                
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            // TODO: Variables :
+
+            bool canIVote = true;
+
+            Console.WriteLine("\n");
+            Console.WriteLine("The biggest Integer : {0}", int.MaxValue);
+            Console.WriteLine("The smallest Integer : {0}", int.MinValue);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("The biggest double : {0}", double.MaxValue);
+            Console.WriteLine("The smallest double : {0}", double.MinValue);
+
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor= ConsoleColor.Black;
+            string playListName = "CoolBands";
+            Console.WriteLine("\n\tMy favorite Spotify playlist is '{0}'", playListName);
 
 
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.Write("Enter Your age: ");
+            int userAge = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine($"Thats cool ! Im also {userAge}");
 
 
             Console.ReadKey();
