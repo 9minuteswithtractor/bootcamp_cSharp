0143c24b8a06c1e89d149acf35a87581e11ed0d9
Peter <>
2023-11-12 22:34:32 +0200

    newFeat : ConsoleColors

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,10 +8,17 @@ namespace FirstApp
         static void Main(string[] args)
         {
             /// TODO: To change Conosle.Color ////////////////
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            /*
+                Console.Clear(); => this would clear the black background and 
+                make it all green
+            */
             ////////////////////////////////////////////////// 
 
+            Console.WriteLine("\n\tHello, World!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("\n\tHello and welcome, all C# devs! :) ");
             Console.ReadKey();
 
