e8284a04d829db46048688944b5765a85a57a8ff
Peter <>
2023-11-12 22:20:30 +0200

    initialize_bootcamp

---
+++ FirstApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FirstApp
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            /// TODO: To change Conosle.Color ////////////////
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            ////////////////////////////////////////////////// 
+
+            Console.WriteLine("\n\tHello and welcome, all C# devs! :) ");
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
+    
+
+    
